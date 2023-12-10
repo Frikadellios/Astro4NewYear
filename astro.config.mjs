e689ec25ce8b4b3c139207ada970d575126d9005
Hrihorii Ilin <devopsick@pm.me>
2023-12-10 19:57:28 +0000

    starting

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,14 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import sveltiaCms from "astro-sveltia-cms";
 import react from "@astrojs/react";
-
 import node from "@astrojs/node";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://devopsick.pages.dev',
   integrations: [mdx(), sitemap(), sveltiaCms(), react()],
-  output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  output: "hybrid",
+  adapter: cloudflare()
 });
